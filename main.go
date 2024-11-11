f2747ab90e523702c00717fbc33e3d02e2ae6c03
angelahuqing <angelahuqing@gmail.com>
2024-11-11 12:52:47 -0800

    [SERV-1173] Update help message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,10 +12,25 @@ import (
 	"github.com/UCLALibrary/pt-tools/cmd/ptrm"
 )
 
+const help = `pt facilitates interactions with a Pairtree without the user needing to know about the Pairtree’s internal structure. 
+
+Please refer to the README(https://github.com/UCLALibrary/pt-tools) for more detailed instructions
+
+	Usage: pt [command] [options]
+	Commands:
+	  ls     List directories and files
+	  rm     Remove files or directories
+	  cp     Copy files or directories
+	  mv     Move files or directories
+	  new    Create a new pairtree object
+	
+	For more information on a specific command, run 'pt [command] --help'.`
+
 func main() {
 	// Basic command-line argument parsing
 	if len(os.Args) < 2 {
-		log.Fatal("No command specified")
+		fmt.Println(help)
+		os.Exit(1)
 	}
 
 	command := os.Args[1]
@@ -28,29 +44,30 @@ func main() {
 	case "ls":
 		err := ptls.Run(args, writer)
 		if err != nil {
-			os.Exit(1)
+			os.Exit(2)
 		}
 	case "rm":
 		err := ptrm.Run(args, writer)
 		if err != nil {
-			os.Exit(1)
+			os.Exit(3)
 		}
 	case "cp":
 		err := ptcp.Run(args, writer)
 		if err != nil {
-			os.Exit(1)
+			os.Exit(4)
 		}
 	case "mv":
 		err := ptmv.Run(args, writer)
 		if err != nil {
-			os.Exit(1)
+			os.Exit(5)
 		}
 	case "new":
 		err := ptnew.Run(args, writer)
 		if err != nil {
-			os.Exit(1)
+			os.Exit(6)
 		}
 	default:
+		fmt.Println(help)
 		log.Fatalf("Unknown command: %s", command)
 	}
 }
